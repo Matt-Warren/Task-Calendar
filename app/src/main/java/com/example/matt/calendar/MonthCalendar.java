485ed0af1bd98d8b7e82fe1768c2b00a38196f30
Matt Warren <warren.matt@hotmail.com>
2016-02-05 17:22:02 -0500

    calendar works lol

--- app/src/main/java/com/example/matt/calendar/MonthCalendar.java
+++ app/src/main/java/com/example/matt/calendar/MonthCalendar.java
@@ -1,13 +1,52 @@
 package com.example.matt.calendar;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MonthCalendar extends AppCompatActivity {
+
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_calendar);
+        initializeCalendar();
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void initializeCalendar(){
+        calendar = (CalendarView) findViewById(R.id.calendar);
+
+        // sets whether to show the week number.
+
+        calendar.setShowWeekNumber(false);
+
+        // sets the first day of week according to Calendar.
+
+        // here we set Monday as the first day of the Calendar
+
+        calendar.setFirstDayOfWeek(2);
+
+
+
+        //sets the listener to be notified upon selected date change.
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        //show the selected date as a toast
+
+            @Override
+
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
