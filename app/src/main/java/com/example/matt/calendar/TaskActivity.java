0b33b24d03bb782cd926b9c9ca1cf4fbcd55e939
Matt Warren <warren.matt@hotmail.com>
2016-02-05 21:31:42 -0500

    some stuff in task done

--- app/src/main/java/com/example/matt/calendar/TaskActivity.java
+++ app/src/main/java/com/example/matt/calendar/TaskActivity.java
@@ -2,12 +2,41 @@ package com.example.matt.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TaskActivity extends AppCompatActivity {
+
+    public int year;
+    public int month;
+    public int day;
+
+    public int startHour;
+    public int duration;
+
+    public String title;
+    public String description;
+
+    EditText taskTitle;
+    EditText taskDesc;
+    EditText taskDur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        Bundle values = getIntent().getExtras();
+
+        year = values.getInt("year");
+        month = values.getInt("month");
+        day = values.getInt("day");
+
+        taskTitle   = (EditText)findViewById(R.id.task_title);
+        taskDesc    = (EditText)findViewById(R.id.task_description);
+        taskDur     = (EditText)findViewById(R.id.task_duration);
+
+
     }
 }
